5705fbdd1c8c630a701a95d7db37c57db07da4d3
hariharan-stark <hariharantony341@gmail.com>
2024-12-13 01:02:40 +0530

    minnor update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MyPortfolio2/', // Set to "/<repo-name>/" if the repo is not named username.github.io
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
